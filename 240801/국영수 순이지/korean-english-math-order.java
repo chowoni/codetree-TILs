07b52c60a5d5cb5fea14302d0701695e17e73151
CHOWON <74358920+chowoni@users.noreply.github.com>
2024-08-01 16:27:40 +0900

    Title: 국영수 순이지, Time: 309ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240801/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+    static class Info {
+        private String name;
+        private int kor;
+        private int eng;
+        private int math;
+
+        public Info(String name, int kor, int eng, int math) {
+            this.name = name;
+            this.kor = kor;
+            this.eng = eng;
+            this.math = math;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getKor(){
+            return kor;
+        }
+
+        public int getEng(){
+            return eng;
+        }
+
+        public int getMath(){
+            return math;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + kor + " " + eng + " " + math;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        ArrayList<Info> infoList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] data = sc.nextLine().split(" ");
+            String name = data[0];
+            int kor = Integer.parseInt(data[1]);
+            int eng = Integer.parseInt(data[2]);
+            int math = Integer.parseInt(data[3]);
+            infoList.add(new Info(name, kor, eng, math));
+        }
+
+        infoList.sort((a, b) -> {
+            if (b.getKor() != a.getKor()) {
+                return b.getKor() - a.getKor();
+            } else if (b.getEng() != a.getEng()) {
+                return b.getEng() - a.getEng();
+            } else if (b.getMath() != a.getMath()) {
+                return b.getMath() - a.getMath();
+            } else {
+                return a.getName().compareTo(b.getName());
+            }
+        });
+        for (Info info : infoList) {
+            System.out.println(info);
+        }
+
+        sc.close();
+    }
+}
