948af2ccadad3617fa2760a786a20e318e38d5d8
CHOWON <74358920+chowoni@users.noreply.github.com>
2024-08-01 09:44:54 +0900

    Title: k번째로 신기한 문자열, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String st = sc.next();
+
+        String[] arr = new String[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.next();
+        
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++){
+            if(arr[i].contains(st))
+                k--;
+            
+            if(k == 0){
+                System.out.print(arr[i]);
+                return;
+            }
+        }
+    }
+}
