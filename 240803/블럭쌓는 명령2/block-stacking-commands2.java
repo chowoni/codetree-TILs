5365b153bb213f00042df6f5603aadd868d0ebb2
CHOWON <74358920+chowoni@users.noreply.github.com>
2024-08-03 00:09:10 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240803/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < k; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for(int j = a; j <= b; j++)
+                arr[j]++;
+        }
+
+        System.out.print(Arrays.stream(arr).max().getAsInt());
+    }
+}
