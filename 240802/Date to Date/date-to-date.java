9a967278442d06b2905625872cb911ff26215998
CHOWON <74358920+chowoni@users.noreply.github.com>
2024-08-02 23:01:35 +0900

    Title: Date to Date, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240802/Date to Date/date-to-date.java
+++ 240802/Date to Date/date-to-date.java
@@ -4,16 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int m1 = sc.nextInt();
-        int d1 = sc.nextInt();
-        int m2 = sc.nextInt();
-        int d2 = sc.nextInt();
+        int m1 = sc.nextInt(); // 시작 월
+        int d1 = sc.nextInt(); // 시작 일
+        int m2 = sc.nextInt(); // 종료 월
+        int d2 = sc.nextInt(); // 종료 일
 
-        int day = (m2*31 + d2) - (m1*31 + d1);
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        if(day == 0)
-         day = 1;
+        int totalDays = 0;
 
-        System.out.print(day);
+        totalDays += daysInMonth[m1 - 1] - d1 + 1;
+
+        if (m1 == m2) {
+            totalDays = d2 - d1 + 1;
+        } else {
+            for (int month = m1; month < m2 - 1; month++) {
+                totalDays += daysInMonth[month];
+            }
+
+            totalDays += d2;
+        }
+
+        System.out.println(totalDays);
     }
 }
