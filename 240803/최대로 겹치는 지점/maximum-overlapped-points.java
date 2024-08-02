d437ce8650d1d028e085c93cef276ceca4a166ea
CHOWON <74358920+chowoni@users.noreply.github.com>
2024-08-03 00:28:19 +0900

    Title: 최대로 겹치는 지점, Time: 250ms, Memory: 10MB, Status: Passed - CodeTree

--- 240803/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 240803/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -1,20 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arr = new int[100];
+        int[] line = new int[101]; // 1부터 100까지의 범위
 
+        // 선분의 정보 입력
         for (int i = 0; i < n; i++) {
             int start = sc.nextInt();
             int end = sc.nextInt();
             for (int j = start; j <= end; j++) {
-                arr[j]++;
+                line[j]++;
             }
         }
 
-        System.out.print(Arrays.stream(arr).max().getAsInt());
+        int maxOverlap = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (line[i] > maxOverlap) {
+                maxOverlap = line[i];
+            }
+        }
+
+        System.out.println(maxOverlap);
     }
 }
