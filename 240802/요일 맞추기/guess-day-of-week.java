e77184065f14eda81166465678b100ccab60d9a3
CHOWON <74358920+chowoni@users.noreply.github.com>
2024-08-02 23:24:55 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240802/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] weekDays = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int baseDayOfWeek = 1; // 0: Sun, 1: Mon, ..., 6: Sat
+
+        int daysBetween = calculateDaysBetween(m1, d1, m2, d2, daysInMonth);
+        int targetDayOfWeek = (baseDayOfWeek + daysBetween) % 7;
+
+        System.out.println(weekDays[targetDayOfWeek]);
+    }
+
+    public static int calculateDaysBetween(int m1, int d1, int m2, int d2, int[] daysInMonth) {
+        int daysFromStart1 = calculateDaysFromStart(m1, d1, daysInMonth);
+        int daysFromStart2 = calculateDaysFromStart(m2, d2, daysInMonth);
+        return daysFromStart2 - daysFromStart1;
+    }
+
+    public static int calculateDaysFromStart(int month, int day, int[] daysInMonth) {
+        int days = 0;
+        for (int i = 0; i < month - 1; i++) {
+            days += daysInMonth[i];
+        }
+        days += day;
+        return days;
+    }
+}
